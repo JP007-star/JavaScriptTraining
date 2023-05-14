47e35de6456d8e9ed33b7eddb22d1ed7b55c2845
JP <justice_of_peace@yahoo.com>
2023-05-14 11:01:33 +0530

    js

--- cookies.js
+++ cookies.js
@@ -1,53 +1,59 @@
-const { log } = require("console");
 
-function createRecord(data) {
-  // Generate a unique identifier for the new record
-  const id = Date.now().toString();
+// function createRecord(data) {
+//   // Generate a unique identifier for the new record
+//   const id = Date.now().toString();
 
-  // Add the record data to a new cookie
-  document.cookie = `${id}=${JSON.stringify(data)}`;
-}
+//   // Add the record data to a new cookie
+//   document.cookie = `${id}=${JSON.stringify(data)}`;
+// }
 
-function readRecord(id) {
-  // Split the cookies string into an array of cookies
-  const cookies = document.cookie.split("; ");
+// function readRecord(id) {
+//   // Split the cookies string into an array of cookies
+//   const cookies = document.cookie.split("; ");
 
-  // Iterate over all the cookies
-  for (const cookie of cookies) {
-    // Split the cookie into a key-value pair
-    const [key, value] = cookie.split("=");
+//   // Iterate over all the cookies
+//   for (const cookie of cookies) {
+//     // Split the cookie into a key-value pair
+//     const [key, value] = cookie.split("=");
 
-    // If the key matches the given identifier, return the value
-    if (key === id) {
-      return JSON.parse(value);
-    }
-  }
+//     // If the key matches the given identifier, return the value
+//     if (key === id) {
+//       return JSON.parse(value);
+//     }
+//   }
 
-  // If no matching cookie was found, return null
-  return null;
-}
+//   // If no matching cookie was found, return null
+//   return null;
+// }
 
-function updateRecord(id, data) {
-  // Find the cookie that matches the given identifier
-  const cookie = readCookie(id);
+// function updateRecord(id, data) {
+//   // Find the cookie that matches the given identifier
+//   const cookie = readCookie(id);
 
-  // If the cookie was found, update its value with the new data
-  if (cookie) {
-    document.cookie = `${id}=${JSON.stringify(data)}`;
-  }
-}
+//   // If the cookie was found, update its value with the new data
+//   if (cookie) {
+//     document.cookie = `${id}=${JSON.stringify(data)}`;
+//   }
+// }
 
-function deleteRecord(id) {
-  // Find the cookie that matches the given identifier
-  const cookie = readCookie(id);
+// function deleteRecord(id) {
+//   // Find the cookie that matches the given identifier
+//   const cookie = readCookie(id);
 
-  // If the cookie was found, remove it by setting its expiry date to the past
-  if (cookie) {
-    document.cookie = `${id}=; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
-  }
-}
-document.cookie='name=jp'
+//   // If the cookie was found, remove it by setting its expiry date to the past
+//   if (cookie) {
+//     document.cookie = `${id}=; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+//   }
+// }
+localStorage.setItem('name','jp')
+sessionStorage.setItem('name',"jp")
 
-const ele=document.getElementById('demo')
-console.log(document.cookie)
-ele.innerHTML=document.cookie
+log(localStorage.getItem(name))
+
+document.cookie = 'username=John Doe';
+
+
+
+document.cookie = "myKey=myValue; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+
+document.getElementById("demo").innerHTML=document.cookie;
